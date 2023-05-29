fcc1ba0624f8d4dc42c5bcecd9d304f1ececb204
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-05-29 05:22:10 +0300

    Added debounce

--- components/Searchbar/Searchbar.jsx
+++ components/Searchbar/Searchbar.jsx
@@ -3,11 +3,16 @@ import style from "./Searchbar.module.css";
 import searchIcon from "@/public/search-icon.svg";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import { debounce } from "throttle-debounce";
 
 export default function Searchbar() {
   const [focused, setFocused] = useState(false);
-  const router = useRouter()
+  const router = useRouter();
+
+  const submitSearch = useCallback(debounce(500, (nameStartsWith) => {
+    router.push(`?nameStartsWith=${nameStartsWith}`);
+  }), []);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,9 +21,17 @@ export default function Searchbar() {
   };
 
   return (
-    <form className={style.main} onSubmit={(event) => handleSubmit(event)} data-focus={focused}>
+    <form className={style.main} onSubmit={(event) => handleSubmit(event)} data-focus={focused} >
       <label hidden htmlFor="searchbar-input">Search a Marvel Character</label>
-      <input id="searchbar-input" name="searchbar-input" placeholder="Search a character" onFocus={() => setFocused(true)} onBlur={() => setFocused(false)}></input>
+      <input
+        id="searchbar-input"
+        name="searchbar-input"
+        placeholder="Search a character"
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onChange={(event) => submitSearch(event?.target?.value)}
+        autoComplete="off"
+      ></input>
       <button type="submit">
         <Image alt="search icon" src={searchIcon} width={16} height={"auto"} />
       </button>
