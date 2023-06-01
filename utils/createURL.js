d5b325f2e7042f32beb3e1487901936798bde63a
Kagan Ahmet Okan <kaganAhmetOkan@gmail.com>
2023-06-01 08:11:08 +0300

    Added Limit env variable

--- utils/createURL.js
+++ utils/createURL.js
@@ -3,20 +3,21 @@ import md5 from "md5";
 const publicKey = process.env.PUBLIC_KEY;
 const privateKey = process.env.PRIVATE_KEY;
 const baseURL = process.env.BASE_URL;
+const limit = process.env.LIMIT;
 
 export default function createURL({ nameStartsWith, titleStartsWith, page, id, comics }) {
   if (!publicKey || !privateKey || !baseURL) return;
 
+  const defaultLimit = 20;
   const url = new URL(baseURL);
   const ts = Date.now();
   const hash = md5(ts + privateKey + publicKey);
-  const limit = 20;
-  const offset = (Number.parseInt(page ?? 1) - 1) * limit;
+  const offset = (Number.parseInt(page ?? 1) - 1) * Number.parseInt(limit ?? defaultLimit);
 
   url.searchParams.set("ts", ts);
   url.searchParams.set("apikey", publicKey);
   url.searchParams.set("hash", hash);
-  url.searchParams.set("limit", limit);
+  url.searchParams.set("limit", limit ?? defaultLimit);
 
   if (nameStartsWith) url.searchParams.set("nameStartsWith", nameStartsWith);
   if (titleStartsWith) url.searchParams.set("titleStartsWith", titleStartsWith);
